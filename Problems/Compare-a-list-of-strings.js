5bf8074536a474f9c700aece4641d512d34bfb11
”Joonas <joonas.kinn@gmail.com>
2020-06-25 22:59:51 +0300

    Add solution to Count occurrences of a substring Problem

---
+++ Problems/Compare-a-list-of-strings.js
@@ -0,0 +1,23 @@
+/*
+Given a list of arbitrarily many strings, implement a function for each of the following conditions:
+
+test if they are all lexically equal
+test if every string is lexically less than the one after it (i.e. whether the list is in strict ascending order) */
+
+function allEqual(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            return false
+        }
+    }
+    return true
+}
+
+function azSorted(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] >= arr[i + 1]) {
+            return false
+        }
+    }
+    return true
+}
