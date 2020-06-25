357b17efee9d85f18309bf6c30e4c5d464eb8ee4
”Joonas <joonas.kinn@gmail.com>
2020-06-25 20:07:03 +0300

    Add solution to Generate lower case ASCII alphabet Problem

---
+++ Problems/Generate-lower-case-ASCII-alphabet.js
@@ -0,0 +1,14 @@
+/* Write a function to generate an array of lower case ASCII characters for a given range.
+For example, given the range ['a', 'd'], the function should return ['a', 'b', 'c', 'd']. */
+
+function lascii(cFrom, cTo) {
+    const asciiFrom = cFrom.charCodeAt(0)
+    const asciiTo = cTo.charCodeAt(0)
+    const arr = []
+    for(let i = asciiFrom; i <= asciiTo; i++) {
+        arr.push(String.fromCharCode(i))
+    }
+    return arr
+}
+
+lascii('a','d')
