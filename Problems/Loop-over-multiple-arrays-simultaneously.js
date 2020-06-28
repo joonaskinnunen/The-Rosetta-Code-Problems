e8728d9f5b56805111536406af405c1fc3eaca60
”Joonas <joonas.kinn@gmail.com>
2020-06-28 18:28:07 +0300

    Add solution to Loop over multiple arrays simultaneously Problem

---
+++ Problems/Loop-over-multiple-arrays-simultaneously.js
@@ -0,0 +1,22 @@
+/* Loop over multiple arrays and create a new array whose  ith  element is the concatenation of  ith  element of each of the given.
+
+For this example, if you are given this array of arrays:
+
+[ ["a", "b", "c"], ["A", "B", "C"], [1, 2, 3] ]
+
+the output should be:
+
+["aA1","bB2","cC3"]
+
+Write a function that takes an array of arrays as a parameter and returns an array of strings satisfying the given description. */
+
+function loopSimult(A) {
+    let newArr = []
+    for (let i = 0; i < A.length; i++) {
+        for (let j = 0; j < A[i].length; j++) {
+            newArr[j] ? newArr[j] = newArr[j] + A[i][j] : newArr[j] = A[i][j]
+        }
+    }
+    return newArr
+}
+loopSimult([["a", "b", "c"], ["A", "B", "C"], [1, 2, 3]])
