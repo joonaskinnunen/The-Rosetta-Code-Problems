01f8ea7004c0433a8cf64d5a9338a7faed96850b
”Joonas <joonas.kinn@gmail.com>
2020-07-03 00:58:42 +0300

    Add solution to Split a character string based on change of character Problem

---
+++ Problems/Split-a-character-string-based-on-change-of-character.js
@@ -0,0 +1,21 @@
+/* Split a (character) string into comma (plus a blank) delimited strings based on a change of character (left to right).
+Blanks should be treated as any other character (except they are problematic to display clearly).
+The same applies to commas. For instance, the string:
+"gHHH5YY++///\"
+should be split as:
+["g", "HHH", "5", "YY", "++", "///", "\" ] */
+
+function split(str) {
+    const arr = []
+    let arrStr = str.charAt(0)
+    for (let i = 1; i <= str.length; i++) {
+        if (str.charAt(i) === arrStr.charAt(arrStr.length - 1)) {
+            arrStr += str.charAt(i)
+        } else {
+            arr.push(arrStr)
+            arrStr = str.charAt(i)
+        }
+    }
+    return arr
+}
+split("sssmmmaaammmaaat")
